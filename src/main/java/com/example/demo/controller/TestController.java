79a6447f62cc44aba24304b4e070195022f08fe5
jun02160 <jun020216@naver.com>
2023-06-13 09:57:55 +0900

    [9~10장] PostMan 테스트 및 @PathVariable 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +14,8 @@ public class TestController {
 		return "Hello World!(안녕하세요!)";
 	}
 	
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+		return "Hello World! ID " + id;
+	}
 }
