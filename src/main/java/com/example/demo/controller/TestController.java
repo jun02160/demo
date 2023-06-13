787ad24d69cfdff140f593560f9aea2e45f00418
jun02160 <jun020216@naver.com>
2023-06-13 10:01:56 +0900

    [12장] @RequestBody 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.TestRequestBodyDTO;
 
 @RestController
 @RequestMapping("test")
@@ -23,5 +26,10 @@ public class TestController {
 	@GetMapping("/Param")
 	public String testControllerWithRequestParam(@RequestParam(required = false) int id) {
 		return "Hello World! ID Param " + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+		return "Hello World! ID" + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
 	}
 }
