8ad567d4fed48ae9e41b45677e0808d9eab13742
jun02160 <jun020216@naver.com>
2023-06-13 10:14:17 +0900

    [14장] ResponseEntity로 HTTP Status 조작하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,7 +21,7 @@ public class TestController {
 	
 	@GetMapping
 	public String testController() {
-		return "Hello World!(안녕하세요!)";
+		return "Hello World!";
 	}
 	
 	@GetMapping("/{id}")
@@ -46,5 +47,15 @@ public class TestController {
 		
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return response;
+	}
+	
+	@GetMapping("/testResponseEntityBad")
+	public ResponseEntity<?> testControllerResponseEntityBad() {
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm ResponseEntity. And you get 400!");
+		list.add("See you!");
+		
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.badRequest().body(response);
 	}
 }
