fc8353ac0aadc74bdf54549c8369a93501b88e63
jun02160 <jun020216@naver.com>
2023-06-13 10:18:58 +0900

    [15장] @Service 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+import com.example.demo.service.TodoService;
 
 
 @RestController
 @RequestMapping("test")
 public class TestController {
+	
+	@Autowired
+	private TodoService service;
 	
 	@GetMapping
 	public String testController() {
@@ -57,5 +63,15 @@ public class TestController {
 		
 		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
 		return ResponseEntity.badRequest().body(response);
+	}
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo() {
+		String str = service.testService();
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
 	}
 }
